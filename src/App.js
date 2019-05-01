5643423165d5a9dabfe9fc3344c438056d8d31e6
Fran Dieguez <fran.dieguez@mabishu.com>
2019-05-01 19:15:44 +0200

    Add React router to play with multiple components and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
+
+  renderHome = () => <h1>Home</h1>
+
+  renderCustomerContainer = () => <h1>Customer Container</h1>
+
+  renderCustomerListContainer = () => <h1>Customer List Container</h1>
+
+  renderCustomerNewContainer = () => <h1>Customer New Container</h1>
+
   render() {
     return (
       <Router>
-      <div className="App">
-        <ul>
-          <li>
-            <Link to="/">Root</Link>
-          </li>
-          <li>
-            <Link to="/customers">Customers</Link>
-          </li>
-          <li>
-            <Link to="/customers/3000">Customers 3000</Link>
-          </li>
-        </ul>
-      </div>
+        <div>
+          <Route exact path="/" component={this.renderHome} />
+          <Route exact path="/customers" component={this.renderCustomerListContainer} />
+          <Switch>
+            <Route exact path="/customers/new" component={this.renderCustomerNewContainer} />
+            <Route exact path="/customers/:dni" component={this.renderCustomerContainer} />
+          </Switch>
+        </div>
       </Router>
     );
   }
