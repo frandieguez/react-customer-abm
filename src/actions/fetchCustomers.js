a3cc73646cb91d641bc53a1b5f8a2e2a7b3a01ea
Fran Dieguez <fran.dieguez@mabishu.com>
2019-05-01 23:57:23 +0200

    Make the fetchCustomers return a hardcoded array of customers

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,4 +1,13 @@
 import { FETCH_CUSTOMERS } from './../constants';
 import { createAction } from 'redux-actions';
 
-export const fetchCustomers = createAction(FETCH_CUSTOMERS)
+const customers = [
+  { name: 'Fran', age: 34, dni: '77409320Z'},
+  { name: 'Sonia', age: 33, dni: '76409320Z'},
+  { name: 'Adrian', age: 36, dni: '35409320Z'}
+];
+
+export const fetchCustomers = createAction(
+  FETCH_CUSTOMERS,
+  () => customers
+)
