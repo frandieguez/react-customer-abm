7b291e9b760fbe7a26215d7487f03561e3847285
Fran Dieguez <fran.dieguez@mabishu.com>
2019-05-01 22:53:53 +0200

    Use material-ui icons in customers listign

--- src/components/CustomerListItem.js
+++ src/components/CustomerListItem.js
@@ -1,20 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { Delete, Edit } from '@material-ui/icons';
 
 const CustomerListItem = ({customer, editAction, delAction, baseUrl}) => {
 
   return (
-    <div>
-      <div className="customer-list-item">
-        <div className="field">
-          <Link to={`${baseUrl}/${customer.dni}`}>{customer.name}</Link>
-        </div>
-        <div className="field">
-          <Link to={`${baseUrl}/${customer.dni}/edit`}>{editAction}</Link>
-        </div>
-        <div className="field">
-          <Link to={`${baseUrl}/${customer.dni}/delete`}>{delAction}</Link>
-        </div>
+    <div className="customer-list-item">
+      <div className="field">
+        <Link to={`${baseUrl}/${customer.dni}`}>{customer.name}</Link>
+      </div>
+      <div className="field">
+        <Link to={`${baseUrl}/${customer.dni}`}><Edit /></Link>
+      </div>
+      <div className="field">
+        <Link to={`${baseUrl}/${customer.dni}/delete`}><Delete /></Link>
       </div>
     </div>
   )
@@ -24,9 +24,7 @@ CustomerListItem.propTypes = {
   customer: PropTypes.shape({
     name: PropTypes.string.isRequired,
     dni: PropTypes.string.isRequired,
-  }).isRequired,,
-  editAction: PropTypes.string.isRequired,
-  delAction: PropTypes.string.isRequired,
+  }).isRequired
 }
 
 export default CustomerListItem
