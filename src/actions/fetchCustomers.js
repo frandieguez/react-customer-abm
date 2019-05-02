4644649e4e76181c6d6e5f12bb150f97ad4a8dfd
Fran Dieguez <fran.dieguez@mabishu.com>
2019-05-02 22:33:31 +0200

    Use a function that retuns a promise in the fetchCustomers provider
    redux-promise will handle it

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,13 +1,10 @@
 import { FETCH_CUSTOMERS } from './../constants';
 import { createAction } from 'redux-actions';
 
-const customers = [
-  { name: 'Alan Moore', age: 34, dni: '123566X'},
-  { name: 'John Doe', age: 33, dni: '234234234Z'},
-  { name: 'Susan Page', age: 36, dni: '342342Y'}
-];
+const url = 'http://localhost:3001/customers';
+const apiFetchCustomers = () => fetch(url).then(v => v.json())
 
 export const fetchCustomers = createAction(
   FETCH_CUSTOMERS,
-  () => customers
+  apiFetchCustomers
 )
