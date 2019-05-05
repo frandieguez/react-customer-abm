5aed6ad73592235105a3ed7291389f6edbb352cf
Fran Dieguez <fran.dieguez@mabishu.com>
2019-05-05 19:16:07 +0200

    Add new api put to update customers

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,11 @@ const url = 'http://localhost:3001/';
 export const apiFetchCustomers = () => fetch(`${url}customers`).then(v => v.json())
 
 export const apiGet = (dni) => fetch(`${url}/customers/${dni}`).then(v => v.json())
+
+export const apiPut = (id, customer) => {
+  return fetch(`${url}customers/${id}`, {
+    method: 'PUT',
+    headers: new Headers({ 'Content-Type' : 'application/json'}),
+    body: JSON.stringify(customer)
+  }).then(v => v.json())
+}
