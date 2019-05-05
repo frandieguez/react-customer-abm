03dffc853d4857a8207a31c03c32343992cf3e47
Fran Dieguez <fran.dieguez@mabishu.com>
2019-05-05 18:54:14 +0200

    Load customers on refresh of customer form

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -6,9 +6,15 @@ import CustomerData from '../components/CustomerData';
 import PropTypes from 'prop-types';
 import { getCustomerByDni } from '../selectors/customers';
 import CustomerEdit from '../components/CustomerEdit';
-
+import { fetchCustomers } from '../actions/fetchCustomers';
 
 class CustomerContainer extends Component {
+
+  componentDidMount() {
+    if (!this.props.customer) {
+      this.props.fetchCustomers();
+    }
+  }
 
   handleSubmit = values => {
     console.log(JSON.stringify(values));
@@ -41,10 +47,13 @@ class CustomerContainer extends Component {
 CustomerContainer.propTypes = {
   dni: PropTypes.string.isRequired,
   customer: PropTypes.object.isRequired,
+  fetchCustomers: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state, props) => ({
   customer: getCustomerByDni(state, props)
 })
 
-export default withRouter(connect(mapStateToProps, null)(CustomerContainer));
+export default withRouter(connect(mapStateToProps, {
+  fetchCustomers
+})(CustomerContainer));
